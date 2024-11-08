5876a3a1daf101ca102c9fcc23fa90feeb998d6f
viestamayriska <vristamayriska@gmail.com>
2024-11-08 21:37:26 +0700

    Jobsheet 10 - Praktikum 4

---
+++ app/Http/Controllers/Api/LevelController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\LevelModel;
+
+class LevelController extends Controller
+{
+    public function index()
+    {
+        return LevelModel::all();
+    }
+
+    public function store(Request $request)
+    {
+        $level = LevelModel::create($request->all());
+        return response()->json($level, 201);
+    }
+
+    public function show(LevelModel $level)
+    {
+        return $level;
+    }
+
+    public function update(Request $request, LevelModel $level)
+    {
+        $level->update($request->all());
+        return response()->json($level, 200);
+    }
+
+    public function destroy(LevelModel $level)
+    {
+        $level->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data terhapus'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 */
+
+// level
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+
+
 
 Route::post('/register', [App\Http\Controllers\Api\RegisterController::class, '__invoke'])->name('register');
 Route::post('/login', [App\Http\Controllers\Api\LoginController::class, '__invoke'])->name('login');
